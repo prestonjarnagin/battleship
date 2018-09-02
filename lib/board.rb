7ce640d90cb1c7c303b4102d6ae1cdc247bac4f7
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-09-01 19:12:31 -0600

    Write basic board tests. Write basic board methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,35 @@
+require './lib/slot'
+
 class Board
+
+  attr_reader :slots
+
+  def initialize
+    @slots = generate_slots
+  end
+
+    def generate_slots(columns = 4, rows = 4)
+      # Keep in mind this method will generate a grid that is accessed with
+      # a [y][x] pattern, since the spec specifies lookup like "A1", where
+      # "A" is the y cordinate
+      board = []
+      i = 0
+      while i < rows
+        board << generate_row(columns)
+        i += 1
+      end
+      return board
+    end
+
+    def generate_row(length)
+      i = 0
+      row = []
+      while i < length
+        row << Slot.new
+        i += 1
+      end
+      return row
+    end
+
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,13 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/board'
 
 class BoardTest < Minitest::Test
 
-  def test_it_exists
-    board = Board.new
-    assert_instance_of Board, board
+  def setup
+    @board = Board.new
   end
 
-  
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_it_has_a_grid_of_slots
+    @board.slots.each do |column|
+      column.each do |item|
+        assert_instance_of Slot, item
+      end
+    end
+  end
+
+  def test_its_grid_defaults_to_4x4
+    assert_instance_of Slot, @board.slots[3][3]
+    refute @board.slots[4]
+    refute @board.slots[3][4]
+  end
 
 end
