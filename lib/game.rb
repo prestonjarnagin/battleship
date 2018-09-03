ea5a238438d7a76117f9808ecf253c82f6b183b7
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-09-03 09:50:02 -0600

    Finish translate_cordinate

--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,7 @@ class Game
     vertical = [true, false].sample
     @board.place_ship(y,x,vertical,3)
 
-    
+
     y = rand.(0..3)
     x = rand(0..3)
     vertical = [true, false].sample
@@ -29,7 +29,31 @@ class Game
     The first is two units long and the
     second is three units long.
     The grid has A1 at the top left and D4 at the bottom right."
+    entry = translate_cordinate(gets.chomp)
+    while !entry
+      p "Invalid cordinate"
+      entry = translate_cordinate(gets.chomp)
+    end
     @board.place_ship(cordinates)
+  end
+
+  def translate_cordinate(cordinate_string)
+    if cordinate_string.length > 2
+      return nil
+    end
+    y = cordinate_string[0].upcase.ord - 65
+    x = cordinate_string[1].to_i - 1
+    if y > @board.length || x > @board[0].length
+      return nil
+    end
+    return [y,x]
+  end
+
+  def validate_ship_placement(cordinates)
+    #Check if all cordinates are on board
+
+
+    #Check if cordinates are next to each other vertically or horizontally
   end
 
 
