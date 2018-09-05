0d78323686eeb61e12fd0509628687dbe158a862
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-09-04 18:27:25 -0600

    Remove useless continuous_slots method from board

--- test/board_test.rb
+++ test/board_test.rb
@@ -58,9 +58,4 @@ class BoardTest < Minitest::Test
     assert_equal 1, @board.ships.length
   end
 
-  def test_it_can_return_continuous_slots
-    assert_equal 16, @board.continuous_slots.length
-    assert_equal @board.slots[0][0], @board.continuous_slots[0]
-  end
-
 end
